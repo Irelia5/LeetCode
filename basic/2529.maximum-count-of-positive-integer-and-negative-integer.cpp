58346fd63cb43187b73409070c0d58ffab64dc12
Irelia <52132456+Irelia5@users.noreply.github.com>
2024-01-23 11:35:57 +0800

    2529

---
+++ basic/2529.maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=2529 lang=cpp
+ * @lcpr version=30113
+ *
+ * [2529] 正整数和负整数的最大计数
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int less = lower_bound(nums , 0);
+        int great = nums.size() - lower_bound(nums, 1);
+        return max(less, great);
+    }
+    int lower_bound(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return left;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [-2,-1,-1,1,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [-3,-2,-1,0,0,1,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [5,20,66,1314]\n
+// @lcpr case=end
+
+ */
+
