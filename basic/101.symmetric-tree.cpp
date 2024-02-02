ac20e8bdf610a6f91fe2b259e3773a91c79569d2
Irelia <52132456+Irelia5@users.noreply.github.com>
2024-02-02 14:52:10 +0800

    101

---
+++ basic/101.symmetric-tree.cpp
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=cpp
+ * @lcpr version=30114
+ *
+ * [101] 对称二叉树
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true;
+        return isSymm(root->left, root->right);
+    }
+
+    bool isSymm(TreeNode* p, TreeNode* q) {
+        if (!p || !q) return p == q;
+        return p->val == q->val && isSymm(p->left, q->right) && isSymm(p->right, q->left);
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,2,3,4,4,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,2,null,3,null,3]\n
+// @lcpr case=end
+
+ */
+
