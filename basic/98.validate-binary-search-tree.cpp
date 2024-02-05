5ed1510538fdd5ec9f9b4251283affee750df8d7
Irelia <52132456+Irelia5@users.noreply.github.com>
2024-02-05 16:37:24 +0800

    98

---
+++ basic/98.validate-binary-search-tree.cpp
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=cpp
+ * @lcpr version=30116
+ *
+ * [98] 验证二叉搜索树
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // pre-order-traversal
+    bool isValidBST(TreeNode* root) {
+        return isValidBSTPre(root, LONG_MIN, LONG_MAX);
+    }
+
+    bool isValidBSTPre(TreeNode* root, long left, long right) {
+        if (!root) return true;
+        long x = root->val;
+        return x < right
+               && x > left
+               && isValidBSTPre(root->left, left, x)
+               && isValidBSTPre(root->right, x, right);
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [2,1,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [5,1,4,null,null,3,6]\n
+// @lcpr case=end
+
+ */
+
