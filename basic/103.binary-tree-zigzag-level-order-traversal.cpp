d6fbb27e1c70c7860e998cf1c493776cf46b4758
Irelia <52132456+Irelia5@users.noreply.github.com>
2024-02-28 16:12:51 +0800

    103

---
+++ basic/103.binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode.cn id=103 lang=cpp
+ * @lcpr version=30117
+ *
+ * [103] 二叉树的锯齿形层序遍历
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if (!root) return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        bool is_even = false;
+        for (; !q.empty(); is_even = !is_even) {
+            vector<int> vals;
+            for (int i = q.size(); i > 0; --i) {
+                auto node = q.front();
+                q.pop();
+                vals.push_back(node->val);
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            if (is_even) reverse(vals.begin(), vals.end());
+            ans.push_back(vals);
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [3,9,20,null,null,15,7]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+ */
+
