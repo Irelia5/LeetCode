2d3a57b9e7ec151fa26aff3f856b1bbcb1c279bb
Irelia <52132456+Irelia5@users.noreply.github.com>
2024-03-05 17:11:22 +0800

    46

---
+++ basic/46.permutations.cpp
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode.cn id=46 lang=cpp
+ * @lcpr version=30117
+ *
+ * [46] 全排列
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        vector<int> path(n);
+        vector<int> on_path(n);
+        
+        function<void(int)> dfs = [&] (int i) {
+            if (i == n) {
+                ans.emplace_back(path);
+                return;
+            }
+            for (int j = 0; j < n; ++j) {
+                if (!on_path[j]) {
+                    path[i] = nums[j];
+                    on_path[j] = true;
+                    dfs(i + 1);
+                    on_path[j] = false;
+                }
+            }
+        };
+        dfs(0);
+        return ans;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+ */
+
