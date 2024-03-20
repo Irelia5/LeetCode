c8780bb181ab5116333a472b0e696db62b38d480
Irelia <52132456+Irelia5@users.noreply.github.com>
2024-03-20 11:01:59 +0800

    70

--- basic/70.climbing-stairs.cpp
+++ basic/70.climbing-stairs.cpp
@@ -27,8 +27,39 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
-    int climbStairs(int n) {
+    int climbStairs1(int n) {
+        if (n <= 1) return 1;
+        return climbStairs1(n - 1) + climbStairs1(n - 2);
+    }
 
+    int climbStairs2(int n) {
+        vector<int> cache(n + 1, 0);
+        function<int(int)> dfs = [&](int i) {
+            if (i <= 1) return 1;
+            auto& res = cache[i];
+            if (res) return res;
+            return res = dfs(i- 1) + dfs(i - 2);
+        };
+        return dfs(n);
+    }
+
+    int climbStairs3(int n) {
+        vector<int> cache(n + 1, 0);
+        cache[0] = 1, cache[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            cache[i] = cache[i - 2] + cache[i - 1];
+        }
+        return cache[n];
+    }
+
+    int climbStairs(int n) {
+        int f_0 = 1, f_1 = 1;
+        for (int i = 2; i <= n; ++i) {
+            int new_f = f_0 + f_1;
+            f_0 = f_1;
+            f_1 = new_f;
+        }
+        return f_1;
     }
 };
 // @lc code=end
